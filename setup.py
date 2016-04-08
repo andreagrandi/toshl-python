4e3cc31f3b481035ee62faa5f7b4a46281f3f5da
Andrea Grandi <a.grandi@gmail.com>
2016-04-08 10:35:16 +0100

    Fix package and convert README to rst format

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='toshl',
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/andreagrandi/toshl-python',
-    download_url='https://github.com/andreagrandi/toshl-python/tarball/0.0.1',
+    download_url='https://github.com/andreagrandi/toshl-python/tarball/0.0.2',
     author='Andrea Grandi',
     author_email='a.grandi@gmail.com',
     description='Python client library for Toshl API.',
