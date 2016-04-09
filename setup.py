28a7e17746db311b5111e6c63d2b8e3edf95334c
Andrea Grandi <a.grandi@gmail.com>
2016-04-09 12:58:47 +0100

    Bump library version to 0.0.3 and remove unnecessary dependencies

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='toshl',
-    version='0.0.2',
+    version='0.0.3',
     url='https://github.com/andreagrandi/toshl-python',
-    download_url='https://github.com/andreagrandi/toshl-python/tarball/0.0.2',
+    download_url='https://github.com/andreagrandi/toshl-python/tarball/0.0.3',
     author='Andrea Grandi',
     author_email='a.grandi@gmail.com',
     description='Python client library for Toshl API.',
@@ -14,8 +14,6 @@ setup(
     platforms='any',
     license='MIT',
     install_requires=[
-        'mock==1.3.0',
-        'pytest==2.9.1',
         'requests==2.9.1',
     ],
 )
